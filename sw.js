e225cbdea3b49db80e8ac3ed4cdab99f58626c37
LimSeongWoo <53285591+LimSeongWoo@users.noreply.github.com>
2026-05-31 20:38:13 +0900

    bump cache v3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'coding-shorts-v2';
+const CACHE = 'coding-shorts-v3';
 const SHELL = ['./', 'index.html', 'manifest.json', 'icon-192.png', 'icon-512.png'];
 
 self.addEventListener('install', (e) => {
